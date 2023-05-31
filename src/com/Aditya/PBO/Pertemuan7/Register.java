58c5d8398dc02186404d99cc8b8cfb9e684bb316
AdityaS034 <21102034@ittelkom-pwt.ac.id>
2023-05-31 15:50:35 +0700

    Pertemuan 7

---
+++ src/com/Aditya/PBO/Pertemuan7/Register.java
@@ -0,0 +1,15 @@
+package com.Aditya.PBO.Pertemuan7;
+
+import javax.swing.*;
+
+public class Register {
+    private JTextField textField1;
+    private JTextField textField2;
+    private JComboBox comboBox1;
+    private JRadioButton lakiLakiRadioButton;
+    private JRadioButton perempuanRadioButton;
+    private JTextArea YANGBENERAWOAKAWOKOAOWAKAWTextArea;
+    private JButton saveButton;
+    private JButton clearButton;
+    private JTextField textField3;
+}
